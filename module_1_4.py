81d656bf5b05d73bd5cc3b2fa2baae1b7bb65198
Nikit71 <nikitural@gmail.com>
2024-10-12 22:24:49 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+# Организация программ и методы строк
+print('Практическая работа по уроку "Организация программ и методы строк"')
+my_string=input('Введите строку предложение": ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string=my_string.replace(' ','')
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
